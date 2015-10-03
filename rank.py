2c2e5781e58de05cb87fd3848652d5f215956ff6
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-03 22:52:02 +0100

    Use with open as foo to manage file opening

--- rank.py
+++ rank.py
@@ -7,11 +7,11 @@ FILE_TYPE = 'html'
 def main():
     ranks = []
     for filename in glob.glob('%s*.%s' % (SCOREBOARD_DIR, FILE_TYPE)):
-        f = open(filename)
-        text = f.read()
+        with open(filename, 'r') as f:
+            text = f.read()
 
-        pass_count = len([m.start() for m in re.finditer('Pass', text)])
-        ranks.append((filename, pass_count))
+            pass_count = len([m.start() for m in re.finditer('Pass', text)])
+            ranks.append((filename, pass_count))
 
     ranks = sorted(ranks, key=lambda x: x[1], reverse=True)
     for rank in ranks:
