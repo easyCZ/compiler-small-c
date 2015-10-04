12904201a531561fb7d35e5852586ed43db3e742
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-04 09:49:23 +0100

    Factor constants out

--- rank.py
+++ rank.py
@@ -4,6 +4,8 @@ import re
 SCOREBOARD_DIR = 'scoreboard/'
 FILE_TYPE = 'html'
 HEADER_ROW_COUNT = 6
+PASS = 'Pass'
+TABLE_ROW = '<tr>'
 
 def main():
     ranks = []
@@ -13,9 +15,9 @@ def main():
             text = f.read()
 
             if not row_count:
-                row_count = len([m.start() for m in re.finditer('<tr>', text)]) - HEADER_ROW_COUNT
+                row_count = len([m.start() for m in re.finditer(TABLE_ROW, text)]) - HEADER_ROW_COUNT
 
-            pass_count = len([m.start() for m in re.finditer('Pass', text)])
+            pass_count = len([m.start() for m in re.finditer(PASS, text)])
             ranks.append((filename, pass_count))
 
     ranks.sort(key=lambda x: x[1], reverse=True)
