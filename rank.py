c82731240a88e1447dc8e2d521483080efbe1d42
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-04 09:47:03 +0100

    Add total number of rows to rank script

--- rank.py
+++ rank.py
@@ -3,19 +3,24 @@ import re
 
 SCOREBOARD_DIR = 'scoreboard/'
 FILE_TYPE = 'html'
+HEADER_ROW_COUNT = 6
 
 def main():
     ranks = []
+    row_count = None
     for filename in glob.glob('%s*.%s' % (SCOREBOARD_DIR, FILE_TYPE)):
         with open(filename, 'r') as f:
             text = f.read()
+
+            if not row_count:
+                row_count = len([m.start() for m in re.finditer('<tr>', text)]) - HEADER_ROW_COUNT
 
             pass_count = len([m.start() for m in re.finditer('Pass', text)])
             ranks.append((filename, pass_count))
 
     ranks.sort(key=lambda x: x[1], reverse=True)
     for rank in ranks:
-        print rank[0].replace(SCOREBOARD_DIR, '').replace('.%s' % FILE_TYPE, ''), rank[1]
+        print rank[0].replace(SCOREBOARD_DIR, '').replace('.%s' % FILE_TYPE, ''), '%i / %i' % (rank[1], row_count)
 
 
 if __name__ == "__main__":
