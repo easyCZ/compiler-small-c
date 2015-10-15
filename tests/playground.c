b9a540dbf16042f12c25f47505f5255a3e3d0321
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-15 19:55:51 +0100

    Grammarz

--- tests/playground.c
+++ tests/playground.c
@@ -5,17 +5,25 @@ int increment(int i) {
 }
 
 
+
+
 void main() {
     int n;
     int i;
     n = read_i();
 
+    n = '\t';
+
     i = 1;
+
+    if (a < b) { return a - increment(i); }
 
     while (i < n) {
         i = increment(i);
         print_i(i);
+        return;
     }
 
+    return;
 }
 
