d3b138b6bd1fec43a8c6401cf5cdc546ce488a21
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-30 17:16:20 +0000

    Make project compile

--- src/sem/NameAnalysisVisitor.java
+++ src/sem/NameAnalysisVisitor.java
@@ -1,6 +1,8 @@
 package sem;
 
 import ast.*;
+import ast.expressions.*;
+import ast.statements.*;
 
 public class NameAnalysisVisitor extends BaseSemanticVisitor<Void> {
 
@@ -31,6 +33,56 @@ public class NameAnalysisVisitor extends BaseSemanticVisitor<Void> {
 	@Override
 	public Void visitVar(Var v) {
 		// To be completed...
+		return null;
+	}
+
+	@Override
+	public Void visitFunctionCallStmt(FunCallStmt funCallStmt) {
+		return null;
+	}
+
+	@Override
+	public Void visitStrLiteral(StrLiteral strLiteral) {
+		return null;
+	}
+
+	@Override
+	public Void visitWhile(While whilez) {
+		return null;
+	}
+
+	@Override
+	public Void visitBinOp(BinOp binOp) {
+		return null;
+	}
+
+	@Override
+	public Void visitIf(If anIf) {
+		return null;
+	}
+
+	@Override
+	public Void visitIntLiteral(IntLiteral intLiteral) {
+		return null;
+	}
+
+	@Override
+	public Void visitChrLiteral(ChrLiteral chrLiteral) {
+		return null;
+	}
+
+	@Override
+	public Void visitReturn(Return aReturn) {
+		return null;
+	}
+
+	@Override
+	public Void visitAssign(Assign assign) {
+		return null;
+	}
+
+	@Override
+	public Void visitFunCallExpr(FunCallExpr funCallExpr) {
 		return null;
 	}
 }
