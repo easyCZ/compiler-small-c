07a14bc7ea0162e965b77390d5220ba46f3bc33c
Milan Pavlik <pavlik.mil@gmail.com>
2015-11-13 21:11:35 +0000

    Add GeneratingClassWriter

--- src/gen/GeneratingClassWriter.java
+++ src/gen/GeneratingClassWriter.java
@@ -1,7 +1,92 @@
 package gen;
 
-/**
- * Created by easy on 13/11/2015.
- */
-public class GeneratingClassWriter {
+import ast.*;
+import ast.expressions.*;
+import ast.statements.*;
+import org.objectweb.asm.ClassWriter;
+
+
+public class GeneratingClassWriter extends ClassWriter implements ASTVisitor {
+
+    private final Program program;
+
+    public GeneratingClassWriter(Program program) {
+        super(COMPUTE_MAXS);
+        this.program = program;
+    }
+
+    @Override
+    public Object visitBlock(Block b) {
+        return null;
+    }
+
+    @Override
+    public Object visitProcedure(Procedure p) {
+        return null;
+    }
+
+    @Override
+    public Object visitProgram(Program p) {
+        return null;
+    }
+
+    @Override
+    public Object visitVarDecl(VarDecl vd) {
+        return null;
+    }
+
+    @Override
+    public Object visitVar(Var v) {
+        return null;
+    }
+
+    @Override
+    public Object visitFunctionCallStmt(FunCallStmt funCallStmt) {
+        return null;
+    }
+
+    @Override
+    public Object visitStrLiteral(StrLiteral strLiteral) {
+        return null;
+    }
+
+    @Override
+    public Object visitWhile(While whilez) {
+        return null;
+    }
+
+    @Override
+    public Object visitBinOp(BinOp binOp) {
+        return null;
+    }
+
+    @Override
+    public Object visitIf(If anIf) {
+        return null;
+    }
+
+    @Override
+    public Object visitIntLiteral(IntLiteral intLiteral) {
+        return null;
+    }
+
+    @Override
+    public Object visitChrLiteral(ChrLiteral chrLiteral) {
+        return null;
+    }
+
+    @Override
+    public Object visitReturn(Return aReturn) {
+        return null;
+    }
+
+    @Override
+    public Object visitAssign(Assign assign) {
+        return null;
+    }
+
+    @Override
+    public Object visitFunCallExpr(FunCallExpr funCallExpr) {
+        return null;
+    }
 }
